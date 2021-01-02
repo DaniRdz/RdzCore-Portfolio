144443beb3957f1b2cf153140d20fcc8b51679df
RdzCore <daniel_rod_con@hotmail.com>
2021-01-02 17:31:33 -0600

    added background image for Home component

--- src/components/home.js
+++ src/components/home.js
@@ -1,10 +1,20 @@
 import React, { Component } from "react";
 
+import backgroundPicture from "../../static/assets/images/backgrounds/home.jpg";
+
 export default class Home extends Component {
   render() {
     return (
       <div className="home">
         <h1>Welcome to my portfolio :D</h1>
+        <div
+          className="background-img"
+          style={{
+            background: "url(" + backgroundPicture + ") no-repeat",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        />
       </div>
     );
   }
