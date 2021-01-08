ee9a2c5da008c8b7abf9852f99a852b86ff53a6a
RdzCore <daniel_rod_con@hotmail.com>
2021-01-08 15:14:02 -0600

    added img styles in portfolio item component

--- src/components/portfolio/portfolio-item.js
+++ src/components/portfolio/portfolio-item.js
@@ -8,7 +8,16 @@ export default class PortfolioItem extends Component {
     const { id, description, thumb_image_url, logo_url } = this.props;
     return (
       <div className="portfolio-item">
-        <div className="item-img">image goes here</div>
+        <div
+          className="item-img"
+          style={{ backgroundImage: `url(${thumb_image_url})` }}
+        />
+        <div className="img-text-wrapper">
+          <div className="logo-wrapper">
+            <img src={logo_url} />
+          </div>
+          <div className="text-decription">{description}</div>
+        </div>
       </div>
     );
   }
