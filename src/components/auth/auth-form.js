e9720945e3e7823e4cdeb955394947c7574bee72
RdzCore <daniel_rod_con@hotmail.com>
2021-01-11 13:11:55 -0600

    generated session with API and catch errors

--- src/components/auth/auth-form.js
+++ src/components/auth/auth-form.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 export default class AuthForm extends Component {
   constructor(props) {
@@ -6,6 +7,7 @@ export default class AuthForm extends Component {
     this.state = {
       email: "",
       password: "",
+      errText: "",
     };
 
     this.handleChange = this.handleChange.bind(this);
@@ -15,11 +17,30 @@ export default class AuthForm extends Component {
   handleChange(event) {
     this.setState({
       [event.target.name]: event.target.value,
+      errText: "",
     });
   }
   handleSubmit(event) {
-    console.log(this.state.email);
-    console.log(this.state.password);
+    axios
+      .post(
+        "https://api.devcamp.space/sessions",
+        {
+          client: { email: this.state.email, password: this.state.password },
+        },
+        { withCredentials: true }
+      )
+      .then((response) => {
+        if (response.data.status === "created") {
+          console.log("you are loggin");
+        } else {
+          this.setState({
+            errText: "Wrong email or password",
+          });
+        }
+      })
+      .catch((err) => {
+        console.log("auth eror", err);
+      });
     event.preventDefault();
   }
   render() {
@@ -28,6 +49,7 @@ export default class AuthForm extends Component {
         <div className="grettigs-msg">Login To Access Your Dashboard</div>
 
         <form onSubmit={this.handleSubmit} className="auth-form-wrapper">
+          <div className="error-text">{this.state.errText}</div>
           <input
             type="email"
             name="email"
