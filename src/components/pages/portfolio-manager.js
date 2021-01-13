e90315791a127fc26c32ce7d0ed22d39aa5507ec
RdzCore <daniel_rod_con@hotmail.com>
2021-01-13 14:06:33 -0600

    created portfolio-mnager layout

--- src/components/pages/portfolio-manager.js
+++ src/components/pages/portfolio-manager.js
@@ -2,6 +2,15 @@ import React, { Component } from "react";
 
 export default class PortfolioManager extends Component {
   render() {
-    return <div className="portfolio-manager">Portfolio Manager goes here</div>;
+    return (
+      <div className="portfolio-manager">
+        <div className="right-column">
+          <h1>Portfolio form goes here</h1>
+        </div>
+        <div className="left-column">
+          <h1>Portfolio sidebar</h1>
+        </div>
+      </div>
+    );
   }
 }
