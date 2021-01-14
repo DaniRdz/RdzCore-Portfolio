83ca3b300fc87ca0ca17acd70e99c71ab90e7697
RdzCore <daniel_rod_con@hotmail.com>
2021-01-14 14:52:54 -0600

    created portfolio side bar component and addes styles

--- src/components/portfolio/portfolio-side-bar-list.js
+++ src/components/portfolio/portfolio-side-bar-list.js
@@ -3,9 +3,14 @@ import React from "react";
 export default function (props) {
   const portfolioList = props.data.map((portfolioItem) => {
     return (
-      <div>
-        <h1>{portfolioItem.name}</h1>
-        <h1>{portfolioItem.id}</h1>
+      <div key={portfolioItem.id} className="portfolio-item-thumb">
+        <div className="portfolio-thumb-img">
+          <img src={portfolioItem.thumb_image_url} />
+        </div>
+        <div>
+          <h1 className="title">{portfolioItem.name}</h1>
+          <h2>{portfolioItem.id}</h2>
+        </div>
       </div>
     );
   });
