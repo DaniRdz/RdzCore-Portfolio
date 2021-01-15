726291ce0adff14e61d7b66addcb6d9156c36ab2
RdzCore <daniel_rod_con@hotmail.com>
2021-01-15 15:35:20 -0600

    created POST method in portfolio manager form

--- src/components/portfolio/portfolio-form.js
+++ src/components/portfolio/portfolio-form.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 export default class PortfolioForm extends Component {
   constructor(props) {
@@ -17,13 +18,34 @@ export default class PortfolioForm extends Component {
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
+  buildForm() {
+    let formData = new FormData();
+    formData.append("portfolio_item[name]", this.state.name);
+    formData.append("portfolio_item[description]", this.state.description);
+    formData.append("portfolio_item[category]", this.state.category);
+    formData.append("portfolio_item[url]", this.state.url);
+    formData.append("portfolio_item[position]", this.state.position);
+
+    return formData;
+  }
   handleChange(event) {
     this.setState({
       [event.target.name]: event.target.value,
     });
   }
   handleSubmit(event) {
-    console.log(event);
+    axios
+      .post(
+        "https://rdzcore.devcamp.space/portfolio/portfolio_items",
+        this.buildForm(),
+        { withCredentials: true }
+      )
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((err) => {
+        console.log("portfolio manager handleSubmit error", err);
+      });
 
     event.preventDefault();
   }
