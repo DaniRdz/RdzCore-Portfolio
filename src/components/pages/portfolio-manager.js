c4f9cf88c278fc30e2a5abdbf73be2e5c9279794
RdzCore <daniel_rod_con@hotmail.com>
2021-01-19 14:05:52 -0600

    created handleDeleteClick funcion and connect with API

--- src/components/pages/portfolio-manager.js
+++ src/components/pages/portfolio-manager.js
@@ -15,6 +15,25 @@ export default class PortfolioManager extends Component {
     this.handleSuccessfulFormSubmission = this.handleSuccessfulFormSubmission.bind(
       this
     );
+    this.handleDeleteClick = this.handleDeleteClick.bind(this);
+  }
+  handleDeleteClick(portfolioItem) {
+    axios
+      .delete(
+        `https://api.devcamp.space/portfolio/portfolio_items/${portfolioItem.id}`,
+        { withCredentials: true }
+      )
+      .then((response) => {
+        this.setState({
+          portfolioItems: this.state.portfolioItems.filter((item) => {
+            return item.id !== portfolioItem.id;
+          }),
+        });
+        return response.data;
+      })
+      .catch((err) => {
+        console.log("handleDeleteClick error", err);
+      });
   }
   handleSuccessfulFormSubmission(portfolioItem) {
     this.setState({
@@ -55,7 +74,10 @@ export default class PortfolioManager extends Component {
           />
         </div>
         <div className="left-column">
-          <PortfolioSideBarList data={this.state.portfolioItems} />
+          <PortfolioSideBarList
+            handleDeleteClick={this.handleDeleteClick}
+            data={this.state.portfolioItems}
+          />
         </div>
       </div>
     );
--- src/components/portfolio/portfolio-side-bar-list.js
+++ src/components/portfolio/portfolio-side-bar-list.js
@@ -10,6 +10,7 @@ export default function (props) {
         <div>
           <h1 className="title">{portfolioItem.name}</h1>
           <h2>{portfolioItem.id}</h2>
+          <a onClick={() => props.handleDeleteClick(portfolioItem)}>Delete</a>
         </div>
       </div>
     );
