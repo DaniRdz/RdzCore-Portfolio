22e7f29344ef38948bb5596d2b240076c29abcb5
RdzCore <daniel_rod_con@hotmail.com>
2021-01-25 14:07:32 -0600

    apply custom Styles

--- src/components/modals/blog-modal.js
+++ src/components/modals/blog-modal.js
@@ -6,10 +6,25 @@ ReactModal.setAppElement(".app-wrapper");
 export default class BlogModal extends Component {
   constructor(props) {
     super(props);
+    this.customStyles = {
+      content: {
+        top: "50%",
+        left: "50%",
+        right: "auto",
+        marginRight: "-50%",
+        transform: "translate(-50%, -50%)",
+        width: "800px",
+        backgroundColor: " #f4f4f4",
+      },
+      overlay: {
+        backgroundColor: "rgba(1, 1, 1, 0.75)",
+      },
+    };
   }
   render() {
     return (
       <ReactModal
+        style={this.customStyles}
         onRequestClose={() => this.props.handleModalClose()}
         isOpen={this.props.modalIsOpen}
       >
