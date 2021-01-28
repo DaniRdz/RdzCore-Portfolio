3002742d7ff0b75dba1d9ac9f2d67ba25fce0ba3
RdzCore <daniel_rod_con@hotmail.com>
2021-01-28 15:19:08 -0600

    config Editor component for images an config base64

--- src/components/forms/rich-text-editor.js
+++ src/components/forms/rich-text-editor.js
@@ -13,6 +13,8 @@ export default class RichTextEditor extends Component {
     };
 
     this.onEditorStateChange = this.onEditorStateChange.bind(this);
+    this.uploadFile = this.uploadFile.bind(this);
+    this.getBase64 = this.getBase64.bind(this);
   }
   onEditorStateChange(editorState) {
     this.setState(
@@ -22,6 +24,17 @@ export default class RichTextEditor extends Component {
       )
     );
   }
+  getBase64(file, callback) {
+    let reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => callback(reader.result);
+    reader.oneerror = (error) => {};
+  }
+  uploadFile(file) {
+    return new Promise((resolve, reject) => {
+      this.getBase64(file, (data) => resolve({ data: { link: data } }));
+    });
+  }
   render() {
     return (
       <div>
@@ -30,6 +43,19 @@ export default class RichTextEditor extends Component {
           wrapperClassName="demo-wrapper"
           editorClassName="demo-editor"
           onEditorStateChange={this.onEditorStateChange}
+          toolbar={{
+            inline: { inDropdown: true },
+            list: { inDropdown: true },
+            textAlign: { inDropdown: true },
+            link: { inDropdown: true },
+            history: { inDropdown: true },
+            image: {
+              uploadCallback: this.uploadFile,
+              alt: { present: true, mandatory: false },
+              previewImage: true,
+              inputAccept: "image/gif,image/jpeg,image/jpg,image/png,image/svg",
+            },
+          }}
         />
       </div>
     );
