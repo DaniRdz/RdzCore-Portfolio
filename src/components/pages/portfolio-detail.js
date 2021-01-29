5583ca9b27e692703dbae67be4218fdaa9fa093c
RdzCore <daniel_rod_con@hotmail.com>
2021-01-29 15:10:29 -0600

    render portfolio item in portfolio detail component

--- src/components/pages/portfolio-detail.js
+++ src/components/pages/portfolio-detail.js
@@ -1,13 +1,46 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 export default class PortfolioDetail extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      portfolioItem: {},
+    };
+  }
+  getPortfolioItem() {
+    axios
+      .get(
+        `https://rdzcore.devcamp.space/portfolio/portfolio_items/${this.props.match.params.slug}`,
+        { withCredentials: true }
+      )
+      .then((response) => {
+        this.setState({
+          portfolioItem: response.data.portfolio_item,
+        });
+      })
+      .catch((err) => {
+        console.log("getPortfolioItem Error", err);
+      });
+  }
+
+  componentDidMount() {
+    this.getPortfolioItem();
   }
   render() {
+    const {
+      banner_image_url,
+      category,
+      description,
+      logo_url,
+      name,
+      thumb_image_url,
+      url,
+    } = this.state.portfolioItem;
     return (
       <div>
-        <h1>Portfolio Details for {this.props.match.params.slug}</h1>
+        <h2>{name}</h2>
+        <p>{description}</p>
       </div>
     );
   }
