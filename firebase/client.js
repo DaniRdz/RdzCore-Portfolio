9af1ca8e939af0527aea9154ea67313959209c4e
RdzCore <daniel_rod_con@hotmail.com>
2021-05-13 19:49:04 -0500

    fix warigs in console for firebase

--- firebase/client.js
+++ firebase/client.js
@@ -1,4 +1,5 @@
-import firebase from "firebase";
+import firebase from "firebase/app";
+import "firebase/firebase-firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBmdFOKd0MzeXxwZ3gZHtQEm-uFJP7n1-U",
